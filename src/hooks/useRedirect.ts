682713762cee36e8511288b47d14a81bd42a4a6b
dukjjang <jhd7292@naver.com>
2022-12-23 00:14:11 +0900

    feat: 리다이렉트 hook에서  error 페이지 제외

--- src/hooks/useRedirect.ts
+++ src/hooks/useRedirect.ts
@@ -1,13 +1,14 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 
 const useRedirect = () => {
+  const { pathname } = useLocation();
   const navigate = useNavigate();
   const isToken = localStorage.getItem('token');
 
   useEffect(() => {
     if (isToken) navigate('/todo');
-    else navigate('/');
+    else if (!isToken && pathname === '/todo') navigate('/');
   }, [isToken, navigate]);
 };
 
