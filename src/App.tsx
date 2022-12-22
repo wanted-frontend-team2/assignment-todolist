3e2204d240d280f75570339cebdc1ed387276a2e
dukjjang <jhd7292@naver.com>
2022-12-22 18:52:20 +0900

    feat: SignForm, SignInput 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Auth from './pages/Auth';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route index element={<Auth />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
